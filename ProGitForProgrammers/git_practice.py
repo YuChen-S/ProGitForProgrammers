4b25e6de5630da87faea2e5811b9222ccf7b59f8
Yu-Chen Shen <65219219+YuChen-S@users.noreply.github.com>
2023-01-07 17:48:45 +0800

    Add func_2 from Lisa

--- ProGitForProgrammers/git_practice.py
+++ ProGitForProgrammers/git_practice.py
@@ -3,3 +3,6 @@ print('The change from second user')
 def func_1_first_user():
     message = 'The first user is Sam'
     return message
+def func_2_second_user():
+    message = 'The second user is Lisa'
+    return message
